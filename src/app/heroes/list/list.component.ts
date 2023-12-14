ba9b9577e8301959c150de69f41188e8f9837182
Virginia Aranda <arandavirginia692@gmail.com>
2023-12-14 17:17:51 -0300

    Fin de sección 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
